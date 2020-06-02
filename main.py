05a31ba53c4c3c0e27770264f8bcc37d5194d8d2
Adrian Muresan <muresan.adrian.bn@gmail.com>
2020-06-02 19:11:03 +0200

    Added spiro-spirograph example

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ t_space = np.linspace(0, 100 * np.pi, 10000)
 pl.plot_parametric(Spirograph(radius=7, domain_len_frac=-1, fraction=1.5), t_space)
 
 ## How about a spiro-spirograph?
-# s = Spirograph(radius=7, domain_len_frac=1, domain_norm_frac=-1.0)
-# s1 = Spirograph(domain_func=s, radius=3, domain_len_frac=1, fraction=1.7)
-# pl.plot_parametric(s1, t_space)
+s = Spirograph(radius=7, domain_len_frac=1, domain_norm_frac=-1.0)
+s1 = Spirograph(domain_func=s, radius=3, domain_len_frac=1, fraction=1.7)
+pl.plot_parametric(s1, t_space)
 
 
